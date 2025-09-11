25cdf4939255f29077562df84672efac7462480a
OriC28 <orianacolina.perea@gmail.com>
2025-09-10 23:37:51 -0400

    add one-hot encoding function

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 from pydataprep import cleaning as cl
+from pydataprep import preprocessing as pr
 import pandas as pd
 
 
-df = pd.read_excel("./data/datatest.xlsx")
-data = df.loc[1]
-header = list(df.columns.values)
+df = pd.read_excel("./data/datacategories.xlsx")
 
-print(cl.remove_duplicates(df))
+# print(cl.remove_duplicates(df))
+print(pr.encode_categorical(df[["Equipo"]], "one_hot"))
--- pydataprep/preprocessing.py
+++ pydataprep/preprocessing.py
@@ -1,9 +1,17 @@
+from sklearn.preprocessing import OneHotEncoder, LabelEncoder
 import pandas as pd
 
 
 def encode_categorical(df: pd.DataFrame, method: str):
     """"Applies one-hot or label-ending encoding to transform categorical variables into a numeric format"""
-    pass
+    if method == "one_hot":
+        encoder = OneHotEncoder(handle_unknown='ignore', sparse_output=False)
+        encoded_items = encoder.fit_transform(df)
+        return pd.DataFrame(encoded_items, columns=encoder.get_feature_names_out(df.columns))
+    elif method == "label":
+        pass
+    else:
+        return "Method not supported."
 
 
 def scale_numerical(df: pd.DataFrame, columns: list, technique: str):
